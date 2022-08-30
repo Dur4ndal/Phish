5d0a9e6a0aaf74409188e7d93afb8b667a5456f7
Dur4ndal <56807764+Dur4ndal@users.noreply.github.com>
2022-08-30 01:38:37 -0400

    Add files via upload

---
+++ post.py
@@ -0,0 +1,7 @@
+import requests,sys
+
+file = open(sys.argv[1],'rb')
+
+test = requests.post('https://localhost:3333/api/groups/', file, verify=False, headers={'Authorization': 'b9df0908f9fb99e4397a32d96cf2348413fd7e7b6b2600bf8e26a93fdcf78c29'})
+file.close()
+print(test.text)
